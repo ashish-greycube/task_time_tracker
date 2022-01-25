b7b901addbc1a38e5e3fe50d4e8dc2297f35604d
ashish-greycube <mr.ashish.shah@gmail.com>
2022-01-25 11:31:20 +0530

    menu

--- task_time_tracker/public/js/todo.js
+++ task_time_tracker/public/js/todo.js
@@ -3,10 +3,10 @@ frappe.ui.form.on("ToDo", {
 		if(frm.doc.reference_type=='Quotation' && frm.doc.reference_name && frm.doc.repair_status_offerte_cf) {
 			frm.add_custom_button(__('Start'), function() {
 				frm.set_value('task_start__date_time_cf', frappe.datetime.now_datetime())
-			});
+			},__('Time'));
 			frm.add_custom_button(__('Stop'), function() {
 				frm.set_value('task_end__date_time_cf', frappe.datetime.now_datetime())
-			});			
+			},__('Time'));			
 		}
 	}
 });
